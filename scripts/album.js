98acbbcdcfdef2e5e36f19780024a5d01949e42c
Joshua Zapata <joshuazap@gmail.com>
2016-03-09 14:10:48 -0800

    Add example album objects

---
+++ scripts/album.js
@@ -0,0 +1,50 @@
+// Example Album
+var albumPicasso = {
+   title: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: 'assets/images/album_covers/01.png',
+   songs: [
+       { title: 'Blue', duration: '4:26' },
+       { title: 'Green', duration: '3:14' },
+       { title: 'Red', duration: '5:01' },
+       { title: 'Pink', duration: '3:21'},
+       { title: 'Magenta', duration: '2:15'}
+   ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+// Third Example Album
+var albumBoc = {
+   title: 'Catalog 3',
+   artist: 'Boards of Canada',
+   label: 'Music70',
+   year: '1987',
+   albumArtUrl: 'assets/images/album_covers/Boc1.png',
+   songs: [
+     { title: 'Line Two', duration: '10:43' },
+     { title: 'Drone 18', duration: '6:40' },
+     { title: 'Drone 2', duration: '5:55'},
+     { title: 'Breach Tones', duration: '8:13' },
+     { title: 'Visual Drone 12', duration: '13:21'},
+     { title: 'Stowed Under', duration: '4:15'},
+     { title: 'Powerline', duration: '2:22'},
+     { title: 'Press', duration: '5:08'}
+   ]
+};
