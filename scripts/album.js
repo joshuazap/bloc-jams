c14a99e983c2113fbe814718400d3ade5245cd5f
Joshua Zapata <joshuazap@gmail.com>
2016-03-17 13:33:56 -0700

    Make template easier to read

--- scripts/album.js
+++ scripts/album.js
@@ -55,11 +55,11 @@ var clickHandler = function () {
 var createSongRow = function (songNumber, songName, songLength) {
     
     var template =
-        '<tr class="album-view-song-item">'
-        + '    <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-        + '    <td class="song-item-title">' + songName + '</td>'
-        + '    <td class="song-item-duration">' + songLength + '</td>'
-        + '</tr>',
+        '<tr class="album-view-song-item">' +
+        '    <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + 
+        '    <td class="song-item-title">' + songName + '</td>' + 
+        '    <td class="song-item-duration">' + songLength + '</td>' + 
+        '</tr>',
         
         $row = $(template),
         
